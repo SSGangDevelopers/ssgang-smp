b7448efe5e4aa3a3646f1314af2b62d2c208eb39
SpikeBonjour <triphuongvasa@gmail.com>
2022-05-24 15:55:57 +0700

    Complete project backbone
    Added configuration, localization and some utils.

--- src/main/java/com/gitlab/ssgangdevelopers/SSGangSMP.java
+++ src/main/java/com/gitlab/ssgangdevelopers/SSGangSMP.java
@@ -1,13 +1,70 @@
 package com.gitlab.ssgangdevelopers;
 
+import com.gitlab.ssgangdevelopers.abc.Locale;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SuppressWarnings("unused") // Will remove in the future
 public class SSGangSMP extends JavaPlugin {
 
+	private static SSGangSMP INSTANCE;
+	private static YamlConfiguration config;
+
 	@Override
 	public void onEnable() {
-		super.onEnable();
+		boolean useless;
+		long startTime = System.currentTimeMillis();
+		Logger l = getLogger();
+		l.log(Level.INFO, "Starting plugin initialization...");
+		l.log(Level.INFO, "Loading locales...");
+		Locale lang;
+		if (!getDataFolder().exists()) {
+			useless = getDataFolder().mkdirs();
+		}
+		this.saveResource("config.yml", false);
+		this.saveResource("messages-en.yml", false);
+
+		l.log(Level.INFO, "Loading configuration...");
+		try {
+			File configFile = new File(getDataFolder(), "config.yml");
+			if (!configFile.exists()) {
+				useless = configFile.createNewFile();
+			}
+			config = new YamlConfiguration();
+			config.load(configFile);
+		} catch (IOException | InvalidConfigurationException e) {
+			l.log(Level.SEVERE, "Error loading configuration. The plugin will shut down.");
+			e.printStackTrace();
+			selfDestruct();
+			return;
+		}
+		l.log(Level.INFO, "Configuration loaded.");
+
+		l.log(Level.INFO, "Loading locales '" + config.get("language") + "'...");
+		try {
+			lang = new Locale(new File(getDataFolder(), "messages-" + config.get("language") + ".yml"));
+		} catch (IOException | InvalidConfigurationException e) {
+			l.log(Level.SEVERE, "Error loading locale. The plugin will shut down.");
+			e.printStackTrace();
+			selfDestruct();
+			return;
+		}
+		l.log(Level.INFO, lang.get("plugin.start.localeComplete"));
+
+		l.log(Level.INFO,
+			lang.get(
+				"plugin.start.done",
+				new String[]{"time", (System.currentTimeMillis() - startTime) + "ms"}
+			)
+		);
+		INSTANCE = this;
 	}
 
 	@Override
@@ -15,14 +72,18 @@ public class SSGangSMP extends JavaPlugin {
 		super.onDisable();
 	}
 
-	public SSGangSMP() {
-		super();
-		INSTANCE = this;
-	}
-
-	private static SSGangSMP INSTANCE;
-
 	public static SSGangSMP getInstance() {
 		return INSTANCE;
+	}
+
+	public static YamlConfiguration getConfiguration() {
+		return config;
+	}
+
+	/**
+	 * Disables the plugin.
+	 */
+	public static void selfDestruct() {
+		Bukkit.getPluginManager().disablePlugin(getInstance());
 	}
 }
--- src/main/java/com/gitlab/ssgangdevelopers/utils/StringUtils.java
+++ src/main/java/com/gitlab/ssgangdevelopers/utils/StringUtils.java
@@ -1,2 +1,27 @@
-package com.gitlab.ssgangdevelopers.utils;public class StringUtils {
+package com.gitlab.ssgangdevelopers.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class StringUtils {
+	/**
+	 * Replaces the placeholder in string with their respective provided values.
+	 *
+	 * @param s the input string.
+	 * @param keypair the keypair, organized like this: {@code {"key1", "val1"}, {"key2", "val2"}, ...}
+	 * @return the string with all placeholders replaced to its respective values.
+	 */
+	@NotNull
+	public static String replacePlaceholders(@NotNull String s, @NotNull String[]... keypair) {
+		String res = s;
+		for (String[] kp : keypair) {
+			List<String> rkp = List.of(kp);
+			if (kp.length < 2) {
+				continue;
+			}
+			res = res.replace(String.format("{%s}", rkp.get(0)), rkp.get(1));
+		}
+		return res;
+	}
 }
