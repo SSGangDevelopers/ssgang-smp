35a581088c2a29681a2882c88742f0b65c4f8082
viet2k7tapcode <vietg310807@gmail.com>
2022-06-02 20:40:25 +0700

    Fix workflow

--- scripts/uploadJar/main.js
+++ scripts/uploadJar/main.js
@@ -1,8 +1,8 @@
-import Client from "ssh2-sftp-client";
-import path from "path";
-import fs from "fs";
-import url from "url";
-const dirName = url.fileURLToPath(new URL(".", import.meta.url));
+import Client from 'ssh2-sftp-client';
+import path from 'path';
+import fs from 'fs';
+import url from 'url';
+const dirName = url.fileURLToPath(new URL('.', import.meta.url));
 const sftp = new Client();
 const args = process.argv.slice(2);
 
@@ -13,28 +13,31 @@ sftp
 		username: args[2],
 		password: args[3],
 	})
-	.then(async () => {
-		console.log("Connected to SFTP server");
+	.then(() => {
+		console.log('Connected to SFTP server');
 
-		sftp.list("/plugins").then((list) => {
+		sftp.list('/plugins').then((list) => {
 			const files = list
-				.filter((e) => e.type == "-")
-				.filter((file) => file.name.endsWith(".jar"))
-				.filter((file) => file.name.startsWith("ssgang-smp-"));
+				.filter((e) => e.type == '-')
+				.filter((file) => file.name.endsWith('.jar'))
+				.filter((file) => file.name.startsWith('ssgang-smp-'));
 
-			files.forEach((file) => {
-				await sftp.delete(`/plugins/${file.name}`)
+			files.forEach(async (file) => {
+				await sftp.delete(`/plugins/${file.name}`);
+				console.log(`Deleted ${file.name}`);
 			});
 
+			console.log(`Deleted ${files.length} old file(s)`);
+
 			const projectRootDir = path.dirname(path.dirname(dirName));
-			const libs = path.join(path.join(projectRootDir, "build"), "libs");
+			const libs = path.join(path.join(projectRootDir, 'build'), 'libs');
 			const jarFile = fs
 				.readdirSync(libs)
-				.filter((file) => file.endsWith(".jar"))
-				.filter((file) => file.startsWith("ssgang-smp-"))[0];
+				.filter((file) => file.endsWith('.jar'))
+				.filter((file) => file.startsWith('ssgang-smp-'))[0];
 
 			sftp.put(path.join(libs, jarFile), `/plugins/${jarFile}`).then(() => {
-				console.log("Jar file upload successfully!");
+				console.log('Jar file upload successfully!');
 				process.exit(0);
 			});
 		});
